aa16c0203b39ac9dfe9165996a2182727847a181
Darshna76 <117654044+Darshna76@users.noreply.github.com>
2023-04-13 23:49:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-//your JS code here. If required.
+var fName=document.getElementById("f-name").value;
+var lName=document.getElementById("l-name").value;
+var pno=document.getElementById("p-no").value;
+var email=document.getElementById("email").value;
+function run(){
+	alert(`first name:${fName} Last Name:${lName} Phone Number:${pno} Email ID:${email}`);
+}
+document.getElementById("btn").addEventListener("click",run);
